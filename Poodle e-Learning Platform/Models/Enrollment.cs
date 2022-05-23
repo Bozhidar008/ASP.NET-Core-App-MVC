cbfbbd0153abc313975018af42c367c171cece7f
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-23 18:08:03 +0300

    Add Class Enrollment

---
+++ Poodle e-Learning Platform/Models/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿namespace Poodle_e_Learning_Platform.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public double Grade { get; set; } // optional with class Results for grade display
+    }
+}

e912c6a66ea80fbe9d97e9c21d9cd2a68ef12157
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-24 13:44:37 +0300

    Add Class AuthorizationHelper

---
+++ Poodle e-Learning Platform/Helpers/AuthorizationHelper.cs
@@ -0,0 +1,29 @@
+﻿using Poodle_e_Learning_Platform.Models;
+
+namespace Poodle_e_Learning_Platform.Helpers
+{
+    public class AuthorizationHelper
+    {
+        private const string AuthorizationErrorMessage = "Only admins can create, update or delete a teacher.";
+        private const string AuthenticationErrorMessage = "Authentication failed. Wrong credentials.";
+        private readonly ITeachersService teacherService;
+
+        public AuthorizationHelper(ITeachersService teacherService)
+        {
+            this.teacherService = teacherService;
+        }
+
+        public Teacher tryGetTeacher(string email)
+        {
+            try
+            {
+                return this.teacherService.Get(email);
+            }
+            catch (EntityNotFoundException)
+            {
+
+                throw new AuthorizationException("Invalid email");
+            }
+        }
+    }
+}

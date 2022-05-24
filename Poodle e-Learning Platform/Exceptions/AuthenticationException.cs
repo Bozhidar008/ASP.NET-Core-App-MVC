abd7133265c694fd9ee9ed55eb697345bcd7d0c2
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-24 13:47:27 +0300

    Add Folder Exception and Class AuthenticationException

---
+++ Poodle e-Learning Platform/Exceptions/AuthenticationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Poodle_e_Learning_Platform.Exceptions
+{
+    public class AuthenticationException : ApplicationException
+    {
+        public AuthenticationException()
+        {
+
+        }
+        public AuthenticationException(string message) : base(message)
+        {
+
+        }
+    }
+}

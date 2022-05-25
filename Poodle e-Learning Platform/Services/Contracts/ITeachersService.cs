5a42f8cc2c56be6e237a44ba1933458660b3c776
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-25 22:29:12 +0300

    Add Interface ITeachersService

---
+++ Poodle e-Learning Platform/Services/Contracts/ITeachersService.cs
@@ -0,0 +1,14 @@
+﻿using Poodle_e_Learning_Platform.Models;
+using System.Collections.Generic;
+
+namespace Poodle_e_Learning_Platform.Services.Contracts
+{
+    public interface ITeachersService
+    {
+        List<Teacher> Get();
+        Teacher Get(int id);
+        Teacher Get(string firstName);
+        bool Exists(string email);
+        Teacher Create(Teacher teacher);
+    }
+}

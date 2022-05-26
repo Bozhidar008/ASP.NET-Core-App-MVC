e80ca5366e33769f31fe0d6b520b7635a805c503
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-26 20:11:25 +0300

    Add Class Post

---
+++ Forum/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Post : TextGive
+    {
+      public string Title { get; set; }
+      public List<Comment> Comments { get; set; }
+       public List<Like> Likes { get; set; }
+
+
+    }
+}

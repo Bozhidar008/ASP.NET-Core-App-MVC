ba4a6d5989410e2fe38fa1ea058e56aa393ec825
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-27 22:08:37 +0300

    Add GlobalConstants

---
+++ Poodle e-Learning Platform/Globals/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Poodle_e_Learning_Platform.Globals
+{
+    public static class GlobalConstants
+    {
+        public const int ADMIN_ROLE_ID = 1;
+        public const int TEACHER_ROLE_ID = 2;
+        public const int STUDENT_ROLE_ID = 3;
+    }
+}

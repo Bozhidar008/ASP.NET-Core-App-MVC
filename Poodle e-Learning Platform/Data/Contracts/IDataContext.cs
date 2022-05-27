95299cd9749008c3d78943ce7a7bcecb841a75e6
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-27 21:55:11 +0300

    Add Class IDataContext

---
+++ Poodle e-Learning Platform/Data/Contracts/IDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poodle_e_Learning_Platform.Models;
+
+namespace Poodle_e_Learning_Platform.Data.Contracts
+{
+    public interface IDataContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<SectionPage> SectionPages { get; set; }
+
+        void SaveChanges();
+    }
+}

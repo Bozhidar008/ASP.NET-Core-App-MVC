8150e972a8fb7531b4fe1e9a792db73a2e91dedd
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-27 21:59:50 +0300

    Add IdentityHostingStartup.cs

---
+++ Poodle e-Learning Platform/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Poodle_e_Learning_Platform.Data;
+
+[assembly: HostingStartup(typeof(Poodle_e_Learning_Platform.Areas.Identity.IdentityHostingStartup))]
+
+namespace Poodle_e_Learning_Platform.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            /*
+            builder.ConfigureServices((context, services) => {
+            services.AddDbContext<ApplicationDbContext>(
+                );
+
+                 
+            });
+            */
+
+        }
+    }
+}
+

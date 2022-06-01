a798ab2c4d2a9eef8197e12153cd1447b8668a62
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-01 11:08:36 +0300

    Add Class UserMapper

---
+++ Forum/Models/Mappers/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication1.Dtos;
+
+namespace WebApplication1.Models.Mappers
+{
+	public class UserMapper
+	{ 
+		public User ConvertToModel(UserRequestDto dto)
+		{
+			User modelUser = new User();
+			modelUser.FirstName = dto.FirstName;
+			modelUser.LastName= dto.LastName;
+			modelUser.UserName = dto.UserName;
+			modelUser.Email = dto.Email;
+			return modelUser;
+		}
+
+	
+	}
+}

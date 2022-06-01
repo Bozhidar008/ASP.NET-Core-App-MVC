3a346aa08d4fd1c4e94b98f698daf89774d23709
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-01 11:09:26 +0300

    Add Class PostMapper

---
+++ Forum/Models/Mappers/PostMapper.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.Dtos;
+
+namespace WebApplication1.Models.Mappers
+{
+    public class PostMapper
+    {
+
+		public Post ConvertToModel(PostRequestDto dto)
+		{
+			Post modelPost = new Post();
+			modelPost.Title = dto.Title;
+			modelPost.Content = dto.Content;
+						
+			return modelPost;
+		}
+	}
+}

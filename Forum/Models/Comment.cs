5ce679b57a27df2bba7e8e54599f2e41b6ff18a0
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-01 11:05:51 +0300

    Add Class Comment Model

---
+++ Forum/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public int PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}

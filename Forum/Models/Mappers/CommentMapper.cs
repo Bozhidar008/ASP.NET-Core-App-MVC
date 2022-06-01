249719196099fc831eb5326db93a695199b73ca0
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-01 11:09:55 +0300

    Add Class CommentMapper

---
+++ Forum/Models/Mappers/CommentMapper.cs
@@ -0,0 +1,20 @@
+﻿using WebApplication1.Dtos;
+
+
+namespace WebApplication1.Models.Mappers
+{
+    public class CommentMapper
+    {
+        public Comment ConvertToModel(CommentRequestDto dto) 
+        {
+            Comment comment = new Comment
+            {
+                Content = dto.Content,
+                PostId = dto.PostId,
+                UserId = dto.UserId,
+            };
+
+            return comment;
+        }
+    }
+}

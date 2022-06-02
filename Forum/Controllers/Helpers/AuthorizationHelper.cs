3acf8765137b1731bee7693c16e3a74809edfbe0
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-02 11:41:14 +0300

    Add Class AuthorizationHelper

---
+++ Forum/Controllers/Helpers/AuthorizationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.Helpers
+{
+    public class AuthorizationHelper
+    {
+        private readonly IUsersService usersService;
+
+        public AuthorizationHelper(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public User TryGetUser(string username)
+        {
+            try
+            {
+                return this.usersService.GetByUsername(username);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Invalid Username");
+            }
+        }
+    }
+}

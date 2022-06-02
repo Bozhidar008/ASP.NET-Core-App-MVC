5523449468053d3ef8c4f1f137f1903c299ffef1
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-02 11:43:02 +0300

    Add Class UnAuthorizedOperationException

---
+++ Forum/Exceptions/UnAuthorizedOperationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Exceptions
+{
+	public class UnAuthorizedOperationException : ApplicationException
+	{
+		public UnAuthorizedOperationException(string message)
+			: base(message)
+		{
+		}
+	}
+}

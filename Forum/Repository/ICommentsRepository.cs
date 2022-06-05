6a2544ce0208325c2d57d8d0c78dbd49b2fb47d3
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-05 22:46:01 +0300

    Add Class ICommentrepository

---
+++ Forum/Repository/ICommentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repository
+{
+    public interface ICommentsRepository
+    {
+        public List<Comment> Get(int postId);
+        public Comment GetById(int commentId);
+
+        public Comment Create(Comment comment);
+
+        public Comment Update(int id, Comment comment, Comment commentToUpdate);
+
+        public Comment Delete(int id,Comment commentToDelete);
+
+
+    }
+}

963f2519184e30ab3e46d4ecb11c30d67a2c0b7f
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-06 21:01:18 +0300

    Add Class IUsersService

---
+++ Forum/Services/IUsersService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Dtos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+   public interface IUsersService
+    {
+		public List<User> GetAll();
+
+		public List<User> Get(UserQueryParameters filterParameters);
+
+		public User GetByUsername(string username);
+		public User GetById(int id);
+
+		public User GetByEmail(string email);
+
+		public User Create(User user);
+
+		public User Update(int id, User userToUpdate, User user);
+
+		public void Delete(int id);
+
+		bool HasUser(string userName);
+		
+	}
+}

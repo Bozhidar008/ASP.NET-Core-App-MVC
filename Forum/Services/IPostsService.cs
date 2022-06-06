d2ae33e9df25e7bbe03d69662b261684bc83879c
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-06 21:02:05 +0300

    Add Class IPostsService

---
+++ Forum/Services/IPostsService.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IPostsService
+    {
+        public List<Post> Get();
+
+        public Post Create(Post post, User user);//?? User user
+
+        public Post Update(int id, Post post, User user);
+
+        public Post Delete(int id, User user);
+    }
+}

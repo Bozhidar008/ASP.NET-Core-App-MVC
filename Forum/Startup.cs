705acabef6af1b29db2d398c7f1d0334745cc024
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-06 21:07:16 +0300

    Add Changes in StartUp

--- Forum/Startup.cs
+++ Forum/Startup.cs
@@ -1,44 +1,72 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using WebApplication1.Controllers;
+using WebApplication1.Controllers.Helpers;
+using WebApplication1.Data;
+using WebApplication1.Models.Mappers;
+using WebApplication1.Repository;
+using WebApplication1.Services;
 
-namespace Forum
+namespace WebApplication1
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
             services.AddControllers();
+
+
+            services.AddDbContext<ApplicationContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("BMConnection")));
+
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<IUsersService, UsersService>();
+
+            services.AddScoped<IPostsService, PostsService>();
+            services.AddScoped<IPostsRepository, PostsRepository>();
+
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<ICommentsService, CommentsService>();
+
+            services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<ILikesService, LikesService>();
+
+            services.AddTransient<UserMapper>();
+            services.AddTransient<PostMapper>();
+            services.AddTransient<CommentMapper>();
+
+            services.AddTransient<AuthorizationHelper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseDeveloperExceptionPage();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {

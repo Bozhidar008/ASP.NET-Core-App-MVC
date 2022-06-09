a75535d3d5531cfd86f265bdfa712850db3e6082
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-09 22:16:42 +0300

    Add Class UserRequestDTo

---
+++ Forum/Dtos/UserRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Dtos
+{
+    public class UserRequestDto
+    {
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The {0} field is required and must not be an empty string.")]
+		[StringLength(32, MinimumLength = 4, ErrorMessage = "The {0} must be between {2} and {1} characters long.")]
+		public string FirstName { get; set; }
+
+		[StringLength(32, MinimumLength = 4, ErrorMessage = "The {0} must be between {2} and {1} characters long.")]
+		public string LastName{ get; set; }
+
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+
+
+	}
+}

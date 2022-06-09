15871e990ad31b8048642a086f71ce4ef14f6e0f
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-09 22:12:53 +0300

    Add ICommentsService

---
+++ Forum/Services/ICommentsService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Dtos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface ICommentsService
+    {
+        
+        public IEnumerable<CommentInfoDto> GetAllCommentsForPost(int userId);
+        public CommentInfoDto GetCommentById(int id);
+
+        public  Comment Create(Comment comment, User user);
+
+        public Comment UpdateComment(int id, Comment comment, User user);
+
+        public Comment DeleteComment(int id, User user);
+
+        public IEnumerable<CommentDto> GetPostComments(int postId);
+    }
+}

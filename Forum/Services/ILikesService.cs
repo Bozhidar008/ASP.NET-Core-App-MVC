aadd1f2497bb0c0d6978ddc20133733827b2bad1
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-09 22:11:53 +0300

    Add ILikesService

---
+++ Forum/Services/ILikesService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Dtos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface ILikesService
+    {
+        public List<Like> GetAllLikes();
+
+        public Like GetLikeById(int id);
+
+        public Like Create(Like like, User user);
+
+        public Like Update(int id, Like like, User user);
+
+        public Like Delete (int id, User user);
+    }
+}

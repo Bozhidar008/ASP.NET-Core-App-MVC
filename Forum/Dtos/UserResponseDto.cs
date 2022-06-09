7fab0040f1b8820398db7a2657f4f75d0e79f254
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-09 22:17:44 +0300

    Add Class UserResponseDto

---
+++ Forum/Dtos/UserResponseDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Dtos
+{
+    public class UserResponseDto
+    {
+
+        public UserResponseDto(User userModel)
+        {
+
+            this.FirstName = userModel.FirstName;
+            this.LastName = userModel.LastName;
+            this.Email = userModel.Email;
+            this.UserName = userModel.UserName;
+            foreach (var post in userModel.Posts)
+            {
+                this.Posts.Add(post.Title);
+            }
+        }
+
+   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public List<string> Posts { get; set; } = new List<string>(); 
+    }
+}

79950b4a3000551295a34ddaf56bf87e43d01f03
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 18:22:23 +0300

    Add Class PostResponceDto

---
+++ Forum/Dtos/PostResponseDto.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Dtos
+{
+    public class PostResponseDto
+    {
+		public PostResponseDto(Post postModel)
+		{
+            this.Title = postModel.Title;
+            this.Content = postModel.Content;
+            this.Author = postModel.User.UserName;//??
+            this.CommentCount = Comments.Count;//??
+            this.LikeCount = Likes.Count;//??
+		}
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int CommentCount { get; set; }//??
+
+        public int LikeCount { get; set; }//??
+
+        public string Author { get; set; }
+        
+        public List<string> Comments { get; set; } = new List<string>();
+
+        public List<string> Likes { get; set; } = new List<string>();
+        
+
+
+      
+    }
+}

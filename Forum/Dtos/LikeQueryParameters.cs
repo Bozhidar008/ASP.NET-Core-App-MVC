b2c6be540b70d0c1937f5c9fc9d4fa5a780bd2bd
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 18:24:25 +0300

    Add Class LikeQueryParameters

---
+++ Forum/Dtos/LikeQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class LikeQueryParameters
+    {
+        // by PostTitle?? or UserId? 
+        public int PostTitle { get; set; }
+
+        // public int UserId { get; set; }
+        public int PostId { get; set; }
+    }
+}

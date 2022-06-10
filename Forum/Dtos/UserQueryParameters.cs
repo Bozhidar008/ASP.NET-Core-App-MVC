b0261d29fdfd1997a3cbcf015ab277bde32f5be6
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 18:21:57 +0300

    Add Class UserQueryParameters

---
+++ Forum/Dtos/UserQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class UserQueryParameters
+    {
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+    }
+}

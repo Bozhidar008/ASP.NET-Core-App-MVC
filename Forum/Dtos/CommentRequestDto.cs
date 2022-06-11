649ab1b69393eb4d9a04835944ee4a022412b431
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-11 12:49:39 +0300

    Add Class CommentRequestDto

---
+++ Forum/Dtos/CommentRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Dtos
+{
+    public class CommentRequestDto
+    {
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The {0} field is required and must not be an empty string.")]
+        [StringLength(8192, MinimumLength = 32, ErrorMessage = "The {0} must be between {2} and {1} characters long.")]
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+
+        public int PostId { get; set; }
+    }
+}

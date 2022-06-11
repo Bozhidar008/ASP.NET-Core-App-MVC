7b4b604789117d9bc0d84afa1157073c1b26860c
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-11 12:50:31 +0300

    Add Class CommentInfoDto

---
+++ Forum/Dtos/CommentInfoDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Dtos
+{
+    public class CommentInfoDto
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string PostTitle { get; set; }
+
+        public string Author { get; set; }
+
+        public int UserId { get; set; }
+    }
+}

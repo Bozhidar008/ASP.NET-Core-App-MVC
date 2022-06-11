03b97ad35b5c61c31b6e4981bc541ed802243c91
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-11 12:49:07 +0300

    Add Class CommentResponseDto

---
+++ Forum/Dtos/CommentResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Dtos
+{
+    public class CommentResponseDto
+    {
+        //need PostId???
+        //need to use user as parameter??
+        public CommentResponseDto(Comment commentModel)
+        {           
+            this.Content = commentModel.Content;
+            this.Author = commentModel.User.UserName;           
+        }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+    }
+}

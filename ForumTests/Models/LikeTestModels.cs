47e0ac4b7c0963736a229d3172d514a8630e696a
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-12 17:16:01 +0300

    Add LikeTestModels

---
+++ ForumTests/Models/LikeTestModels.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WEbApplication1Tests.Models
+{
+    public class LikeTestModels
+    {
+        
+        public List<Like> GetLikes(List<User> users, List<Post> posts)
+        {
+            return new List<Like>()
+            {
+               new Like()
+               {
+                Id = 1,
+                UserId = 1,
+                PostId = 1
+               },
+               new Like()
+               {
+                Id = 2,
+                UserId = 2,
+                PostId = 2
+               },
+               new Like()
+               {
+                Id = 3,
+                UserId = 1,
+                PostId = 3
+               }
+            };
+        }
+    }
+}

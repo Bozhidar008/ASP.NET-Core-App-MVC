418b16b35910473478fce75724b2f20b01772db8
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-12 17:15:36 +0300

    Add CommentTestModels

---
+++ ForumTests/Models/CommentTestModels.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WEbApplication1Tests.Models
+{
+    public class CommentTestModels
+    {
+        public List<Comment> GetComments(List<Post> posts)
+        {
+            return new List<Comment>()
+            {
+              new Comment()
+              {
+                Id = 1,
+                Content = "First comment",
+                UserId = 1,
+                //PostId = posts[0].Id ???
+                PostId = 1
+              },
+              new Comment()
+              {
+                Id = 2,
+                Content = "Second comment",
+                UserId = 2,
+                PostId = 2
+              },
+              new Comment()
+              {
+                Id = 3,
+                Content = "Third comment",
+                UserId = 1,
+                PostId = 3
+              }
+            };
+        }
+    }
+}

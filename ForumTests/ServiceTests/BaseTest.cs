a952d9ac43d8f3c26a3c133316833291d8d33c39
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-13 18:28:05 +0300

    Add BaseTest

---
+++ ForumTests/ServiceTests/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WEbApplication1Tests.ServicesTests
+{
+    public abstract class BaseTest
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            var options = GetOptions();
+            var context = new ApplicationContext(options);
+            context.Database.EnsureDeleted();
+        }
+
+        public static DbContextOptions<ApplicationContext> GetOptions()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("InMemoryDB")
+                .Options;
+            return options;
+        }
+    }
+}

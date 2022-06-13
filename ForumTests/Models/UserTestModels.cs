e96924ba09ca9bb793b3b9e7da265d12431f8e7c
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-13 18:27:22 +0300

    Add UserTestModel

---
+++ ForumTests/Models/UserTestModels.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WEbApplication1Tests.Models
+{
+    public class UserTestModels
+    {
+        public List<User> GetUsers()
+        {
+            return new List<User>()
+            {
+                new User
+                {
+                  Id = 1,
+                  FirstName = "Georgi",
+                  LastName = "Georgiev",
+                  UserName = "Gosho",
+                  Email = "georgi.georgiev@mail.com",
+                  IsAdmin = true,
+                  IsBlocked = false
+                },
+                new User
+                {
+                  Id = 2,
+                  FirstName = "Petar",
+                  LastName = "Petrov",
+                  UserName = "pesho",
+                  Email = "petar.petrov@mail.com",
+                  IsAdmin = false,
+                  IsBlocked = false               
+                }
+            };
+        }      
+    }
+}

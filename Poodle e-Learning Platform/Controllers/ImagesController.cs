56e60689b3d9c0b25daa5561cb14709b38b2a20c
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-20 10:15:42 +0300

    Add ImagesController

---
+++ Poodle e-Learning Platform/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Poodle_e_Learning_Platform.Services.Contracts;
+
+namespace Poodle_e_Learning_Platform.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private readonly IImageService imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        [HttpGet]
+        public IActionResult GetOriginal(string id)
+        {
+            byte[] content = imageService.GetOrignal(id);
+
+            return File(content, "image/jpeg");
+        }
+
+
+        [HttpGet]
+        public IActionResult GetVisualize(string id)
+        {
+            byte[] content = imageService.GetVisualize(id);
+
+            return File(content, "image/jpeg");
+        }
+
+        [HttpGet]
+        public IActionResult GetThumbnail (string id)
+        {
+            byte[] content = imageService.GetThumbnail(id);
+
+            return File(content, "image/jpeg");
+        }
+    }
+}

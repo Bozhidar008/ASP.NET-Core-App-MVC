3ed92aa0242c655ba4abaa53c2b46dc23897b450
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-21 19:17:51 +0300

    Add ApplicationUserInputModel

---
+++ Poodle e-Learning Platform/InputModels/ApplicationUser/ApplicationUserInputModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Poodle_e_Learning_Platform.InputModels.ApplicationUser
+{
+    public class ApplicationUserInputModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IFormFile Image{ get; set; }
+    }
+}

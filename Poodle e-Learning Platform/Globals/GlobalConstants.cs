65bdd653c11a3da1795ceb63220cc6c76540ede6
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-21 19:12:54 +0300

    Changes in GlobalConstants

--- Poodle e-Learning Platform/Globals/GlobalConstants.cs
+++ Poodle e-Learning Platform/Globals/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const int ADMIN_ROLE_ID = 1;
         public const int TEACHER_ROLE_ID = 2;
         public const int STUDENT_ROLE_ID = 3;
+
+        public const int INDEX_PAGE_ELEMENTS_COUNT = 4;
     }
 }

71e8408b6002374a1bdb67ec2d4c5f44804fe0d9
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-21 19:09:12 +0300

    Add EnrollmentApiController

---
+++ Poodle e-Learning Platform/Controllers/ApiControllers/EnrollmentsApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Poodle_e_Learning_Platform.Services.Contracts;
+
+namespace Poodle_e_Learning_Platform.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsApiController : ControllerBase
+    {
+        private readonly IEnrollmentService enrollmentService;
+
+        public EnrollmentsApiController(IEnrollmentService enrollmentService)
+        {
+            this.enrollmentService = enrollmentService;
+        }
+
+        [Route("IsEnrolled")]
+        [HttpDelete]
+        public IActionResult IsEnrolled(int userId, int courseId)
+        {
+            if (!enrollmentService.IsEnrolled(userId, courseId))
+            {
+                return StatusCode(405);
+            }
+
+            return Ok();
+        }
+    }
+}

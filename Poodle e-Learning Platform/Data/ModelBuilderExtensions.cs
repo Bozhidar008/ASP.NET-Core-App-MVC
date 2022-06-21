3af6ea61b8dc9a9169a1149e2536f2fad098e89d
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-21 19:11:15 +0300

    Add ModelBuilderExtension

---
+++ Poodle e-Learning Platform/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Poodle_e_Learning_Platform.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Poodle_e_Learning_Platform.Data
+{
+    public static class ModelBuilderExtensions
+    {
+
+        public static IEnumerable<Course> courses;
+        
+        static ModelBuilderExtensions()
+        {
+
+            //teachers = SeedModels.GetTeachers();
+           // students = SeedModels.GetStudents();
+           // admins = SeedModels.GetAdmins();
+            //courses = SeedModels.GetCourses();
+           
+        }
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {                     
+            modelBuilder.Entity<Course>().HasData(courses);            
+        }
+    }
+}

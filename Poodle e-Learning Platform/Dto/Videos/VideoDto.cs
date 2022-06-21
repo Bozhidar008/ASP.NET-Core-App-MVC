4f27e6d4431a551c522b7f94c030cad737b5e18e
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-21 19:12:09 +0300

    Add VideoDto

---
+++ Poodle e-Learning Platform/Dto/Videos/VideoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Poodle_e_Learning_Platform.Dto.Videos
+{
+    public class VideoDto
+    {
+        public int SectionId { get; set; }
+
+        public string Url { get; set; }
+    }
+}

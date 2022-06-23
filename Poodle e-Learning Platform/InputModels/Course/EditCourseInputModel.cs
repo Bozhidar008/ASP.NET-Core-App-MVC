6a3f832a2d05dd4ec9315942b6f24c18a9644257
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-23 23:33:19 +0300

    Add EditCourseInputModel

---
+++ Poodle e-Learning Platform/InputModels/Course/EditCourseInputModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Poodle_e_Learning_Platform.InputModels.Course
+{
+    public class EditCourseInputModel : CourseInputModel
+    {
+        public int CourseId { get; set; }
+
+        [Display(Name = "Image")]
+        public IFormFile Image { get; set; }
+    }
+}

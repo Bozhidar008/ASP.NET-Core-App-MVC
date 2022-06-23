081056f03149d6e57a1cc987d25ecc810b887250
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-23 23:34:16 +0300

    Add ImageInputModel

---
+++ Poodle e-Learning Platform/InputModels/Images/ImageInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Poodle_e_Learning_Platform.InputModels.Images
+{
+    public class ImageInputModel
+    {
+        public int UserId { get; set; }
+
+        public string OriginalType { get; set; }
+
+        public string OriginalName { get; set; }
+
+        public Stream ImageData { get; set; }
+    }
+}

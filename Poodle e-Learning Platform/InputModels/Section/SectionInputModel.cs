9b36609d7cb7f8c502025b5fbfb2994e272a55b1
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-23 23:34:45 +0300

    Add SectionInputModel

---
+++ Poodle e-Learning Platform/InputModels/Section/SectionInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poodle_e_Learning_Platform.InputModels.Section
+{
+    public class SectionInputModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        public int CourseId { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        public string Content { get; set; } 
+    }
+}
